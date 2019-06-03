120d9a6ca119a0d7b96ac55d45a0876fdd6fb0e4
junbo <shadow1163>
2019-06-03 11:05:56 +0800

    fix file name

--- logger.go
+++ logger.go
@@ -1,38 +1,55 @@
 package logger
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"runtime"
 )
 
-var Log *logger = NewLogger()
+// Log instance
+var Log = NewLogger()
 
 type logger struct {
 	log *log.Logger
 }
 
+//NewLogger Create log instance
 func NewLogger() *logger {
 	logger := logger{}
-	logger.log = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+	logger.log = log.New(os.Stdout, "", log.Ldate|log.Ltime)
 	return &logger
 }
 
 func (logger *logger) Debug(message ...interface{}) {
-	logger.log.SetPrefix("[DEBUG] ")
+	logger.setPrefix("DEBUG")
 	logger.log.Println(message...)
 }
 
 func (logger *logger) Info(message ...interface{}) {
-	logger.log.SetPrefix("[INFO] ")
+	logger.setPrefix("INFO")
 	logger.log.Println(message...)
 }
 
 func (logger *logger) Warning(message ...interface{}) {
-	logger.log.SetPrefix("[Warning] ")
+	logger.setPrefix("Warning")
 	logger.log.Println(message...)
 }
 
 func (logger *logger) Error(message ...interface{}) {
-	logger.log.SetPrefix("[ERROR] ")
+	logger.setPrefix("ERROR")
 	logger.log.Println(message...)
+}
+
+func (logger *logger) caller(skip int) (pc uintptr, file string, line int, ok bool) {
+	return runtime.Caller(skip)
+}
+
+func (logger *logger) setPrefix(level string) {
+	pc, file, line, ok := logger.caller(3)
+	if !ok {
+		logger.log.Println("Error to get func name and file name!")
+		return
+	}
+	logger.log.SetPrefix(fmt.Sprintf("[%s] %s %s %d ", level, file, runtime.FuncForPC(pc).Name(), line))
 }
